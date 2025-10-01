cabd7b4175c99f09d2ed3b53e31ab216f539ff0f
youke17 <youke177938@gmail.com>
2025-10-01 20:51:02 +0800

    fix typo ( params -> tokens)

--- pwa.config.ts
+++ pwa.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from "vite-plugin-pwa"
 
 const pwaOption: Partial<VitePWAOptions> = {
   includeAssets: ["icon.svg", "apple-touch-icon.png"],
-  filename: "swx.js",
+  filename: "swx.js", 
   manifest: {
     name: "NewsNow",
     short_name: "NewsNow",
